0d0c185e31b93dde6de7ef0689df23741d7842ad
Admin <Admin@LD-F2-R6-S005>
2022-12-27 20:37:58 +0500

    changes

--- TrialApp/WinFormsApp1/Form1.Designer.cs
+++ TrialApp/WinFormsApp1/Form1.Designer.cs
@@ -31,7 +31,6 @@ namespace WinFormsApp1
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -51,19 +50,10 @@ namespace WinFormsApp1
             this.label1.TabIndex = 1;
             this.label1.Text = "Version 4";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(262, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Version 5";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(76, 113);
+            this.label3.Location = new System.Drawing.Point(172, 29);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 13);
             this.label3.TabIndex = 3;
@@ -75,7 +65,6 @@ namespace WinFormsApp1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -89,7 +78,6 @@ namespace WinFormsApp1
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
     }
 }
